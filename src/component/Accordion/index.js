770d3509a959994c9c15d9f220a3a5007f86a83e
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-08-18 22:07:18 -0300

    Feat: modelo de menu lateral adicionado

---
+++ src/component/Accordion/index.js
@@ -0,0 +1,96 @@
+'use client'
+
+import React, { useState } from 'react';
+
+const AccordionItem = ({ title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="border-b border-gray-200">
+      <button
+        onClick={toggleAccordion}
+        className="w-full text-left p-4 bg-gray-100 hover:bg-gray-200 focus:outline-none"
+      >
+        <div className="flex justify-between items-center">
+          <h3 className="text-lg font-semibold text-slate-950">{title}</h3>
+          <span className="text-xl text-slate-950">
+            {isOpen ? '-' : '+'}
+          </span>
+        </div>
+      </button>
+      {isOpen && (
+        <div className="p-4 bg-white text-slate-950">
+          {content}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Accordion = () => {
+  const rh = [
+    "Seleção",
+    "Recrutamento",
+    "Treinamentos",
+    "Desenvolvimento de programas",
+    "Organizar entrevistas",
+    "Coordenar a contratação"
+  ];
+
+  const estoque = [
+    "Fiscalizar a entrada e saída das mercadorias",
+    "Verificar a validade e o estoque dos produtos",
+    "Realizar contagens físicas regulares",
+    "Organizar o local de armazenamento",
+    "Recepcionar as mercadorias",
+    "Distribuir produtos e preparar volumes"
+  ];
+
+  const financeiro = [
+    "Planejamento",
+    "Gestão dos recursos",
+    "Controle",
+    "Tesouraria",
+    "Gestão dos impostos",
+    "Gestão de fluxo de caixa"
+  ];
+
+  const vendas = [
+    "Realizar atendimento a clientes",
+    "Negociar preço e descontos da venda",
+    "Orientar quanto às especificações dos produtos/serviços",
+    "Controlar os pedidos dos clientes",
+    "Informar sobre qualidades dos produtos e prazo de entrega estabelecido",
+    "Auxiliar nas dúvidas dos clientes"
+  ];
+
+
+  return (
+    <div className="max-w-lg mx-auto mt-8">
+
+          <AccordionItem
+            title="RH"
+            content={rh.map((item, i) => <p>{item}</p>)}
+          />
+        )
+      <AccordionItem
+        title="Estoque"
+        content={estoque.map((item, i) => <p>{item}</p>)}
+      />
+      <AccordionItem
+        title="Financeiro"
+        content={financeiro.map((item, i) => <p>{item}</p>)}
+      />
+      <AccordionItem
+        title="Vendas"
+        content={vendas.map((item, i) => <p>{item}</p>)}
+      />
+    </div>
+  );
+};
+
+export default Accordion;
--- src/component/MenuLateral/index.js
+++ src/component/MenuLateral/index.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import './menu.css';
+import Accordion from "@/component/Accordion";
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,14 +11,31 @@ const Menu = () => {
     setIsOpen(!isOpen);
   };
 
+              // className="menu-toggle"
   return (
-    <div>
-      <button onClick={toggleMenu} className="menu-toggle">
-        {isOpen ? 'Fechar Menu' : 'Abrir Menu'}
+    <div className="flex items-center justify-center h-screen">
+      <button
+        className="relative w-10 h-10 focus:outline-none menu-toggle"
+        onClick={toggleMenu}
+      >
+        <span
+          className={`block absolute h-[0.18rem] w-5 bg-black transform transition duration-500 ease-in-out ${isOpen ? 'rotate-45 open' : '-translate-y-2'}`}
+        ></span>
+        <span
+          className={`block absolute h-[0.18rem] w-5 bg-black transform transition duration-500 ease-in-out ${isOpen ? 'opacity-0 open' : 'opacity-100'}`}
+        ></span>
+        <span
+          className={`block absolute h-[0.18rem] w-5 bg-black transform transition duration-500 ease-in-out ${isOpen ? '-rotate-45 open' : 'translate-y-2'}`}
+        ></span>
       </button>
-      <div className={`menu ${isOpen ? 'open' : 'closed'}`}>
-        {/* Coloque o conteúdo do menu aqui */}
-      </div>
+      <nav className={`menu shadow-ambr-500 ${isOpen ? 'open' : 'closed'}`}>
+        <section className={"bg-[var(--principal-color)] p-10"}>
+          <h2 className={"text-center"}>ERP</h2>
+        </section>
+        <section className={""}>
+          <Accordion />
+        </section>
+      </nav>
     </div>
   );
 };
