719f9742cef77ffb74258d2834655d8d98c1527f
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-08-22 20:15:12 -0300

    Init: initial page rh painel

--- src/app/sistema/rh/page.js
+++ src/app/sistema/rh/page.js
@@ -1,13 +1,37 @@
+"use client"
+
 import Filter from "@/component/Filter";
+import NormalButton from "@/component/Element/NormalButton";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFilter, faPlus} from "@fortawesome/free-solid-svg-icons";
+import {useState} from "react";
 // import {metadata} from "@/app/sistema/layout";
 
 // metadata.title = 'Gerenciamento de RH'
 
 
 export default function RHPage() {
+  const [visibleFilter, setVisibleFilter] = useState(false);
   return (
     <main className="flex w-full min-h-screen text-center justify-center bg-[var(--default-bg-dark)] p-4">
-      rh
+      <section className={"max-w-full w-[87rem] gap-6 flex flex-col items-center"}>
+        <section className={"flex justify-end w-full gap-3"}>
+          <NormalButton
+
+            className={"flex items-center gap-2 bg-gray-300 p-2 max-h-[33px]"}
+            icon={<FontAwesomeIcon icon={faFilter}/>}
+            text={<p>Filtrar</p>}
+            onClick={() => setVisibleFilter(!visibleFilter)}/>
+
+        </section>
+        {
+          visibleFilter && (
+            <Filter className={"max-h-10"}/>
+          )
+        }
+
+
+      </section>
     </main>
   );
 }
--- src/component/Element/NormalButton/index.js
+++ src/component/Element/NormalButton/index.js
@@ -3,7 +3,7 @@ export default function NormalButton(props) {
     <button
       type={props.type || "button"}
       onClick={props.onClick}
-      className={`${props.className} shadow flex justify-center font-[600] transition-all duration-500 hover:scale-110`}>
+      className={`${props.className} outline-none shadow flex justify-center font-[600] transition-all duration-500 hover:scale-110`}>
       {props.icon} {props.text}
     </button>
   )
