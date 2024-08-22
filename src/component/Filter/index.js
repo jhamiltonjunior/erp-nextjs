ae506a356aef85ebef7ed097b382c1918654ec9f
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-08-22 20:56:06 -0300

    Fix: z-index added on filter

--- src/component/Filter/index.js
+++ src/component/Filter/index.js
@@ -29,7 +29,7 @@ export default function Filter(props) {
   ];
 
   return (
-    <section className={`filter grid xl:grid-cols-7 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 ${props.className}`}>
+    <section className={`filter z-[100] min-h-fit box-border grid xl:grid-cols-7 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 ${props.className}`}>
       <input type={"text"} className={"filter-input w-full " + classNameInput}/>
 
       <input type={"date"} className={"filter-input w-full " + classNameInput}/>
