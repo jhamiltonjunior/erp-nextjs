57912c625719dd8d41b531ae9aabb498b76e3302
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-08-22 20:34:19 -0300

    Feat: button to aplicate filter

--- src/component/Filter/index.js
+++ src/component/Filter/index.js
@@ -1,6 +1,10 @@
 "use client"
 import './style.css'
 import SelectCustom from "@/component/Element/SelectCustom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck, faSearch} from "@fortawesome/free-solid-svg-icons";
+import NormalButton from "@/component/Element/NormalButton";
+import React from "react";
 
 export default function Filter(props) {
   const classNameInput = "border p-0.5 rounded px-2 focus:border-[var(--principal-color) focus:outline-none]"
@@ -25,7 +29,7 @@ export default function Filter(props) {
   ];
 
   return (
-    <section className={`filter grid xl:grid-cols-6 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 ${props.className}`}>
+    <section className={`filter grid xl:grid-cols-7 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 ${props.className}`}>
       <input type={"text"} className={"filter-input w-full " + classNameInput}/>
 
       <input type={"date"} className={"filter-input w-full " + classNameInput}/>
@@ -34,6 +38,14 @@ export default function Filter(props) {
       <SelectCustom className={"h-full max-w-[178px] min-w-full"} placeholder={"Todas empresas"} options={options} isMulti />
       <SelectCustom className={"h-full max-w-[175px] min-w-full"} placeholder={"Todas as areas"} options={optionsC} isMulti />
       <SelectCustom className={"h-full max-w-[175px] min-w-full"} placeholder={"Ordem"} options={optionsArrow} isClearable />
+
+      <NormalButton
+        type={"submit"}
+        className={"flex items-center gap-2 bg-[var(--principal-color)] p-2 text-white px-2 max-h-[33px] xl:col-span-1 lg:col-span-3 md:col-span-2 sm:col-span-1"}
+        icon={<FontAwesomeIcon icon={faSearch}/>}
+        text={<p>Aplicar</p>}
+        onClick={() => console.log("Pesquisar")}/>
+
 
     </section>
   )
