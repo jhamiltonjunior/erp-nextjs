1ed80416326c2f007c75dc2ce10ed47d0eae1a6f
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-08-25 23:40:01 -0300

    Feat: full calendar added - initial config

---
+++ src/app/sistema/rh/selecao/page.js
@@ -0,0 +1,58 @@
+"use client"
+
+import Filter from "@/component/Filter";
+import {useState} from "react";
+import CardVacancy from "@/component/Vacancy/CardVacancy";
+import NormalButton from "@/component/Element/NormalButton";
+import {faPlus, faFilter} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import SimpleModal from "@/component/SimpleModal";
+import HandleVacancy from "@/component/Vacancy/Handle";
+import Calendar from "@/component/Calendar";
+// import { Metadata } from 'next'
+//
+// export const metadata = {
+//   title: 'Dashboard'
+// }
+export default function SelectionPage() {
+  const [visibleFilter, setVisibleFilter] = useState(false);
+  const [visibleModalHandleVacancy, setVisibleModalHandleVacancy] = useState(false);
+
+
+  return (
+    <main className="flex w-full min-h-screen text-center justify-center bg-[var(--default-bg-dark)] p-4">
+      <section className={"max-w-full w-[87rem] gap-6 flex flex-col items-center"}>
+        <section className={"flex justify-end w-full gap-3"}>
+          <NormalButton
+
+            className={"flex items-center gap-2 bg-gray-300 px-2 max-h-[33px]"}
+            icon={<FontAwesomeIcon icon={faFilter} />}
+            text={<p>Filtrar</p>}
+            onClick={() => setVisibleFilter(!visibleFilter)}/>
+
+          <NormalButton
+
+            className={"flex items-center gap-2 bg-[var(--principal-color)] p-2 text-white px-2 max-h-[33px]"}
+            icon={<FontAwesomeIcon icon={faPlus} />}
+            text={<p>Nova Tipo de Processo</p>}
+            onClick={() => setVisibleModalHandleVacancy(!visibleModalHandleVacancy)}/>
+        </section>
+        {
+          visibleFilter && (
+            <Filter className={""} />
+          )
+        }
+
+        <SimpleModal
+          content={<HandleVacancy />}
+          isOpen={visibleModalHandleVacancy}
+          setIsOpen={setVisibleModalHandleVacancy}/>
+
+        <Calendar />
+
+        {/*<CardVacancy setIsOpenEdit={() => setVisibleModalHandleVacancy(!visibleModalHandleVacancy)} />*/}
+        {/*<CardVacancy setIsOpenEdit={() => setVisibleModalHandleVacancy(!visibleModalHandleVacancy)} />*/}
+      </section>
+    </main>
+  );
+}
---
+++ src/component/Calendar/index.js
@@ -0,0 +1,102 @@
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import {useEffect, useRef} from "react";
+import ptBrLocale from '@fullcalendar/core/locales/pt-br';
+import './style.css'
+
+export default function Calendar({className}) {
+  const calendarRef = useRef(null);
+
+  useEffect(() => {
+    if (calendarRef.current) {
+      const calendarApi = calendarRef.current.getApi();
+      // Você pode manipular a API do calendário aqui se necessário
+    }
+  }, []);
+
+  return (
+    <main className={`calendar w-full  ${className}`}>
+      <FullCalendar
+        className={`w-full max-h-[80px]`}
+        dayMaxEventRows={3}
+        ref={calendarRef}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        editable={true}
+        droppable={true}
+        headerToolbar={{
+          left: 'dayGridMonth timeGridWeek timeGridDay',
+          center: 'title',
+          right: 'today prev next',
+        }}
+        locales={[ptBrLocale]}
+        locale={"pt-BR"}
+        buttonText={{
+          today: "Hoje",
+          month: "Mes",
+          week: "Semana",
+          day: "Dia",
+        }}
+        events={[
+          { title: 'event 1', date: '2024-08-01' },
+          { title: 'event 2', date: '2024-08-02' },
+          { title: 'event 1', date: '2024-08-03' },
+          { title: 'event 2', date: '2024-08-04' },
+          { title: 'event 1', date: '2024-08-05' },
+          { title: 'event 2', date: '2024-08-06' },
+          { title: 'event 1', date: '2024-08-01' },
+          { title: 'event 2', date: '2024-08-02' },
+          { title: 'event 1', date: '2024-08-03' },
+          { title: 'event 2', date: '2024-08-04' },
+          { title: 'event 1', date: '2024-08-05' },
+          { title: 'event 2', date: '2024-08-06' },
+        ]}
+        customButtons={{
+          prev: {
+            text: 'Mes Anterior',
+            click: () => calendarRef.current.getApi().prev(),
+            classNames: ['custom-button'],
+          },
+          next: {
+            text: 'Próximo Mes',
+            click: () => calendarRef.current.getApi().next(),
+            classNames: ['custom-button'],
+          },
+          today: {
+            text: 'Hoje',
+            click: () => calendarRef.current.getApi().today(),
+            classNames: ['custom-button'],
+          },
+          month: {
+            text: 'Mes Anterior',
+            click: () => calendarRef.current.getApi().prev(),
+            classNames: ['custom-button'],
+          },
+        }}
+        initialView="dayGridMonth"
+
+        dateClick={function(info) {
+          console.log('Data clicada: ' + info.dateStr);
+        }}
+
+        drop={ function(info) {
+          alert(info.event.title + " was dropped on " + info.event.start.toISOString());
+
+          if (document.getElementById('drop-remove').checked) {
+            info.event.remove();
+          }
+        }}
+
+        eventClick={function(info) {
+          console.log('Clicked on: ' + info.dateStr);
+          console.log('Clicked on event: ' + info.event.title);
+
+          console.log(info)
+        }}
+
+      />
+    </main>
+  )
+}
