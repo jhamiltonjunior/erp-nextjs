327fcd4d7a2b0da3fe87a63f9077925f64248391
josehamiltonsantosjunior <josehamiltonsantosjunior@gmail.com>
2024-09-06 23:04:52 -0300

    Feat: accordion on menulateral changed to accordion of shadcn

--- src/component/Accordion/index.js
+++ src/component/Accordion/index.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import './style.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChevronUp} from "@fortawesome/free-solid-svg-icons";
+import {Accordion as AccordionUI, AccordionContent, AccordionItem as AccordionItemUI, AccordionTrigger} from "@/component/ui/accordion";
 
 export const AccordionItem = ({ title, content, className, classContentShow}) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -63,10 +64,6 @@ const Accordion = (props) => {
     "Entrada e Saída",
     "Controle Financeiro",
     "Controle Total",
-    "Realizar contagens físicas regulares",
-    "Organizar o local de armazenamento",
-    "Recepcionar as mercadorias",
-    "Distribuir produtos e preparar volumes"
   ];
 
   const estoqueLink = [
@@ -75,10 +72,6 @@ const Accordion = (props) => {
     "entrada-e-saida",
     "controle-financeiro",
     "controle-total",
-    "Realizar contagens físicas regulares",
-    "Organizar o local de armazenamento",
-    "Recepcionar as mercadorias",
-    "Distribuir produtos e preparar volumes"
   ];
 
   const financeiro = [
@@ -120,18 +113,6 @@ const Accordion = (props) => {
 
   return (
     <div className="max-w-lg mx-auto mt-8">
-
-      {/*<AccordionItem*/}
-      {/*  title="Dashboard"*/}
-      {/*  content={rh.map((item, i) =>*/}
-      {/*    <Link className={*/}
-      {/*      "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"*/}
-      {/*    } key={i} href={`/sistema/rh${rhLink[i]}`}>*/}
-      {/*      {item}*/}
-      {/*    </Link>*/}
-      {/*  )}*/}
-      {/*/>*/}
-
       <Link className={
         "menu-item-a block p-4 transform transition-transform text-slate-950 duration-500 hover:translate-x-2 hover:text-white"
         } href={`/sistema`}
@@ -139,51 +120,74 @@ const Accordion = (props) => {
       >
         Dashboard
       </Link>
-      
-      <AccordionItem
-        title="RH"
-        content={rh.map((item, i) =>
-          <Link className={
-            "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"
-          } key={i} href={`/sistema/rh/${rhLink[i]}`}
-                onClick={() => props.setState(!props.state)}>
-            {item}
-          </Link>
-        )}
-      />
-      <AccordionItem
-        title="Estoque"
-        content={estoque.map((item, i) =>
-          <Link className={
-            "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"
-          } key={i} href={`/sistema/estoque/${estoqueLink[i]}`}
-                onClick={() => props.setState(!props.state)}>
-            {item}
-          </Link>
-        )}
-      />
-      <AccordionItem
-        title="Financeiro"
-        content={financeiro.map((item, i) =>
-          <Link className={
-            "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"
-          } key={i} href={`/sistema/financeiro/${financeiroLink[i]}`}
-                onClick={() => props.setState(!props.state)}>
-            {item}
-          </Link>
-        )}
-      />
-      <AccordionItem
-        title="Vendas"
-        content={vendas.map((item, i) =>
-          <Link className={
-            "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"
-          } key={i} href={`/sistema/vendas/${vendasLink[i]}`}
-                onClick={() => props.setState(!props.state)}>
-            {item}
-          </Link>
-        )}
-      />
+
+      <AccordionUI type="single" collapsible className="w-full text-slate-950">
+        <AccordionItemUI value="item-1">
+          <AccordionTrigger className={"p-4"}>RH</AccordionTrigger>
+          <AccordionContent className={"p-4 bg-white"}>
+            {rh.map((item, i) =>
+              <Link className={
+                "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"
+              } key={i} href={`/sistema/rh/${rhLink[i]}`}
+                    onClick={() => props.setState(!props.state)}>
+                {item}
+              </Link>
+            )}
+          </AccordionContent>
+        </AccordionItemUI>
+        <AccordionItemUI value="item-2">
+          <AccordionTrigger className={"p-4"}>Estoque</AccordionTrigger>
+          <AccordionContent className={"p-4 bg-white"}>
+            {estoque.map((item, i) =>
+              <Link className={
+                "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"
+              } key={i} href={`/sistema/estoque/${estoqueLink[i]}`}
+                    onClick={() => props.setState(!props.state)}>
+                {item}
+              </Link>
+            )}
+          </AccordionContent>
+        </AccordionItemUI>
+        <AccordionItemUI value="item-3">
+          <AccordionTrigger className={"p-4"}>Financeiro</AccordionTrigger>
+          <AccordionContent className={"p-4 bg-white"}>
+            {financeiro.map((item, i) =>
+              <Link className={
+                "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"
+              } key={i} href={`/sistema/financeiro/${financeiroLink[i]}`}
+                    onClick={() => props.setState(!props.state)}>
+                {item}
+              </Link>
+            )}
+          </AccordionContent>
+        </AccordionItemUI>
+        <AccordionItemUI value="item-4">
+          <AccordionTrigger className={"p-4"}>Vendas</AccordionTrigger>
+          <AccordionContent className={"p-4 bg-white"}>
+            {vendas.map((item, i) =>
+              <Link className={
+                "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"
+              } key={i} href={`/sistema/vendas/${vendasLink[i]}`}
+                    onClick={() => props.setState(!props.state)}>
+                {item}
+              </Link>
+            )}
+          </AccordionContent>
+        </AccordionItemUI>
+      </AccordionUI>
+
+
+      {/*<AccordionItem*/}
+      {/*  title="RH"*/}
+      {/*  content={rh.map((item, i) =>*/}
+      {/*    <Link className={*/}
+      {/*      "menu-item-a block p-4 transform transition-transform duration-500 hover:translate-x-2 hover:text-white"*/}
+      {/*    } key={i} href={`/sistema/rh/${rhLink[i]}`}*/}
+      {/*          onClick={() => props.setState(!props.state)}>*/}
+      {/*      {item}*/}
+      {/*    </Link>*/}
+      {/*  )}*/}
+      {/*/>*/}
     </div>
   );
 };
